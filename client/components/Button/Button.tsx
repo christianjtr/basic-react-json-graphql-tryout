5687595252ef5db5004feac6a5a176d67d04793a
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-05 01:03:38 +0200

    feat: :sparkles: Add button component

---
+++ client/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    children: string | number;
+    disabled?: boolean;
+    variant?: "primary";
+    isFullWidth?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = (props): React.JSX.Element => {
+    const { children, variant = "primary", disabled = false, isFullWidth = false, ...rest } = props;
+
+    const buttonClasses = [
+        styles.button,
+        styles[variant],
+        ...(isFullWidth ? [styles.fullWidth] : []),
+        ...(disabled ? [styles.disabled] : [])
+    ].filter(Boolean).join(" ");
+
+    return (
+        <button className={buttonClasses} disabled={disabled} {...rest}>{children}</button>
+    );
+}
+
+export { Button };
