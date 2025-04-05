7f29875d5af3124ba04748121071fa06df89b90c
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-05 03:08:43 +0200

    chore: :wrench: Add configuration to redirect to a proper page

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  // /**
+  //  * INFO: Just for assestment purposes...
+  //  * Redirects to a more suitable route convention
+  //  */
+  async redirects() {
+    return [
+      {
+        source: '/product',
+        destination: '/products/1',
+        permanent: false,
+      },
+    ];
+  },
 };
