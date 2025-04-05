a754b3ce315a69f0a1582817882b9b69beafa39c
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-05 03:07:42 +0200

    chore: Add basic product page

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,5 @@
-export default function Product() {
-  return <div>Product page</div>;
+import React from "react";
+
+export default function Product(): React.JSX.Element {
+  return (<div>Product</div>);
 }
