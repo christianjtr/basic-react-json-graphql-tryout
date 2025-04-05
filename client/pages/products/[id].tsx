4c5bc35de02f7034d50ed220e50d951a30ad8f93
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-05 15:11:59 +0200

    feat: :sparkles: Enhance products page

--- client/pages/products/[id].tsx
+++ client/pages/products/[id].tsx
@@ -1,20 +1,28 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useContext } from "react";
 import { useRouter } from "next/router";
 
-import { useProductByIdService } from "../../hooks/useProductByIdService";
+import { Header, Footer } from "../../layout";
 import { Button } from "../../components/Button/Button";
 import { Card } from "../../components/Card/Card";
 import { Spacer } from "../../components/Spacer/Spacer";
 import { QuantitySelector } from "../../components/QuantitySelector/QuantitySelector";
 import { ProductDescription, ProductSpecifications } from "./components";
 import { formatNumberCurrency } from "../../utils/formatNumberCurrency";
+import { useProductByIdService } from "../../hooks/useProductByIdService";
+import { ShoppingCartContext } from "../../contexts/ShoppingCart/ShoppingCartContext";
+import { SHOPPING_CART_ACTION_TYPES } from "../../contexts/ShoppingCart/action-types";
 import styles from "./products.module.css";
+
+const INITIAL_ITEM_COUNT = 1;
 
 export default function ProductDetailsPage(): React.JSX.Element | null {
     const router = useRouter();
     const { id } = router.query;
 
+    const [quantity, setQuantity] = useState<number>(INITIAL_ITEM_COUNT);
+
     const { data } = useProductByIdService(+id!);
+    const { dispatch } = useContext(ShoppingCartContext);
 
     const specifications = useMemo(() => ([
         { label: 'Brand', value: data?.brand },
@@ -26,58 +34,68 @@ export default function ProductDetailsPage(): React.JSX.Element | null {
 
     const productPrice = useMemo(() => formatNumberCurrency(data?.price || 0, undefined, 'GBP'), [data?.price]);
 
-    //TODO: Handle action when an item is added...
-    const handleOnAddItemToCard = () => { }
+    const handleOnSelectQuantity = (quantitySelected: number) => {
+        setQuantity(quantitySelected);
+    }
+
+    const handleOnAddItemToCard = () => {
+        if (quantity === 0) {
+            return;
+        }
+
+        const itemToBeAdded = {
+            qty: quantity,
+            price: data?.price!,
+            total: (data?.price || 0) * quantity,
+            sku: data?.id!
+        };
+
+        dispatch({
+            type: SHOPPING_CART_ACTION_TYPES.ADD_ITEM_TO_CART,
+            payload: {
+                item: itemToBeAdded
+            }
+        });
+
+        setQuantity(INITIAL_ITEM_COUNT);
+    }
 
     if (!data) {
         return null;
     }
 
     return (
-        <div>
-            <div className={styles.container}>
-                {/* __HEADER__ */}
-                {/* TODO: This could be extracted... */}
-                <div className={styles.header}>
-                    <img src={"/octopus-logo.svg"} alt="Octopus Energy Logo" width="230px" />
-                    <img className={styles.cartIcon} src="/basket.svg" alt="Basket icon" />
-                </div>
-                <div className={styles.section}>
-                    <Card>
-                        <Card.Image src={data.img_url} alt="Card image" aria-label="Card image" />
-                        <Card.Title>{data.name}</Card.Title>
-                        <Card.Content>
-                            <span className={styles.subtitle}>
-                                {`${data.power} // Packet of ${data.quantity}`}
-                            </span>
-                            <Spacer />
-                            <div className="flex-container-inline-spaced" style={{ '--alignItems': 'end' } as React.CSSProperties}>
-                                <span className={styles.labelTitle}>{productPrice}</span>
-                                <QuantitySelector />
-                            </div>
-                            <Spacer />
-                            <Button id="addToCart" onClick={handleOnAddItemToCard} aria-label="Add to cart" isFullWidth={true}>
-                                Add to cart
-                            </Button>
-                        </Card.Content>
-                    </Card>
-                </div>
-                <div className={styles.section}>
-                    <ProductDescription>
-                        {data.description}
-                    </ProductDescription>
-                </div>
-                <div className={styles.section}>
-                    <ProductSpecifications data={specifications} />
-                </div>
-                {/* __FOOTER__ */}
-                {/* TODO: This could be extracted... */}
-                <div className={styles.section}>
-                    <div className={styles.footer}>
-                        <p>Octopus Energy Ltd is a company registered in England and Wales. Registered number: 09234424. Registered office: 33 Holborn, London, EC1N 2HT. Trading office: 20-24 Broadwick Street, London, W1F 8HT.</p>
-                    </div>
-                </div>
+        <div className={styles.container}>
+            <Header />
+            <div className={styles.section}>
+                <Card>
+                    <Card.Image src={data.img_url} alt="Card image" aria-label="Card image" />
+                    <Card.Title>{data.name}</Card.Title>
+                    <Card.Content>
+                        <span className={styles.subtitle}>
+                            {`${data.power} // Packet of ${data.quantity}`}
+                        </span>
+                        <Spacer />
+                        <div className="flex-container-inline-spaced" style={{ '--alignItems': 'end' } as React.CSSProperties}>
+                            <span className={styles.labelTitle}>{productPrice}</span>
+                            <QuantitySelector value={quantity} onSelectQuantity={handleOnSelectQuantity} />
+                        </div>
+                        <Spacer />
+                        <Button id="addToCart" onClick={handleOnAddItemToCard} aria-label="Add to cart" isFullWidth={true}>
+                            Add to cart
+                        </Button>
+                    </Card.Content>
+                </Card>
             </div>
+            <div className={styles.section}>
+                <ProductDescription>
+                    {data.description}
+                </ProductDescription>
+            </div>
+            <div className={styles.section}>
+                <ProductSpecifications data={specifications} />
+            </div>
+            <Footer />
         </div>
-    )
+    );
 }
