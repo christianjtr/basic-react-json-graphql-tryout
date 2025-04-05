211f88ba42ce179a227f2614b24c5ecb564b20bc
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-05 03:08:07 +0200

    chore: Adjust type

--- client/components/Button/Button.tsx
+++ client/components/Button/Button.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: string | number;
-    disabled?: boolean;
     variant?: "primary";
     isFullWidth?: boolean;
 }
