3640c52711067ea83949639784e44d02651947bb
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-09 17:16:58 +0200

    chore:

--- client/test/product.test.tsx
+++ client/test/product.test.tsx
@@ -1,6 +1,7 @@
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+import singleProduct from "../__fixtures__/singleProduct.json";
 import { ShoppingCartProvider } from "../contexts/ShoppingCart/ShoppingCartProvider";
 import ProductDetailsPage from "../pages/products/[id]";
 
@@ -15,22 +16,7 @@ jest.mock('next/router', () => ({
 
 jest.mock('../hooks/useProductByIdService', () => ({
   useProductByIdService: jest.fn().mockReturnValue({
-    data: {
-      "id": "1",
-      "name": "Energy saving light bulb",
-      "power": "25W",
-      "description": "Available in 7 watts, 9 watts, 11 watts Spiral Light bulb in B22, bulb switches on instantly, no wait around warm start and flicker free features make for a great all purpose bulb.",
-      "price": 12.99,
-      "quantity": 4,
-      "brand": "Philips",
-      "weight": 77,
-      "height": 12.6,
-      "width": 6.2,
-      "length": 6.2,
-      "model_code": "E27 ES",
-      "colour": "Cool daylight",
-      "img_url": "https://i.ibb.co/2nzwxnQ/bulb.png"
-    }
+    data: singleProduct
   })
 }));
 
